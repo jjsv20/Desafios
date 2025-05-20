5ad44bc9acf2b4c5938a5d112d55e68e9faf4016
jjsv20 <jorim.svillamizar@udea.edu.co>
2025-05-20 14:58:18 -0500

    login

--- UdeAStay/main.cpp
+++ UdeAStay/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+bool iniciarSesion(string& nombreUsuario, string& tipoRol){
+    string usuario, contraseña;
+    cout << "\nIngrese nombre de usuario: ";
+    cin >> usuario;
+    cout << "Ingrese contraseña: ";
+    cin >> contraseña;
+    ifstream archivo("usuarios.txt");
+    string linea;
+    while(getline(archivo, linea)){
+        size_t p1 = linea.find(',');
+        size_t p2 = linea.find(',', p1 + 1);
+        string usuarioArchivo = linea.substr(0, p1);
+        string passwordArchivo = linea.substr(p1 + 1, p2 - p1 - 1);
+        string rolArchivo = linea.substr(p2 + 1);
+        if(usuario == usuarioArchivo && contraseña == passwordArchivo){
+            tipoRol = rolArchivo;
+            nombreUsuario = usuario;
+            return true;
+        }
+    }
+    cout << "Usuario o contraseña incorrectos.";
+    return false;
+}
 
 int main()
 {
     cout << "Hello World!" << endl;
+    string usuario, rol;
+    if(iniciarSesion(usuario, rol)){
+        if(rol == "H"){
+            cout << "Bienvenido " << usuario;
+        }
+    }
     return 0;
 }
