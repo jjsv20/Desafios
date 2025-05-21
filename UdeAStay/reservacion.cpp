aae28add611c96e3d7f4dd2d1182cba956d6878b
jjsv20 <jorim.svillamizar@udea.edu.co>
2025-05-21 18:45:51 -0500

    class reservacion

--- UdeAStay/reservacion.cpp
+++ UdeAStay/reservacion.cpp
@@ -1,3 +1,63 @@
 #include "reservacion.h"
+#include <cstring>
 
-Reservacion::Reservacion() {}
+Reservacion::Reservacion(const char* fechaEn, int durac, const char* codigoRese, Alojamiento* alojam, Usuario* huesp, const char* metodoP, const char* fechaP, float mont, const char* anota) {
+    fechaEntrada = new char[strlen(fechaEn) + 1];
+    strcpy(fechaEntrada, fechaEn);
+    duracion = durac;
+    codigoReservacaion = new char[strlen(codigoRese) + 1];
+    strcpy(codigoReservacaion, codigoRese);
+    alojamiento = alojam;
+    huesped = huesp;
+    metodoDePago = new char[strlen(metodoP) + 1];
+    strcpy(metodoDePago, metodoDePago);
+    fechadePago = new char[strlen(fechaP) + 1];
+    strcpy(fechadePago, fechaP);
+    monto = mont;
+    anotaciones = new char[strlen(anota) + 1];
+    strcpy(anotaciones, anota);
+}
+
+Reservacion::~Reservacion(){
+    delete[] fechaEntrada;
+    delete[] codigoReservacaion;
+    delete[] metodoDePago;
+    delete[] fechadePago;
+    delete[] anotaciones;
+}
+
+const char* Reservacion::getFechaEntrada() const {
+    return fechaEntrada;
+}
+
+int Reservacion::getDuracion() const {
+    return duracion;
+}
+
+const char* Reservacion::getCodigoReservacion() const {
+    return codigoReservacion;
+}
+
+Alojamiento* Reservacion::getAlojamiento() const {
+    return alojamiento;
+}
+
+Usuario* Reservacion::getHuesped() const {
+    return huesped;
+}
+
+const char* Reservacion::getMetodoPago() const {
+    return metodoPago;
+}
+
+const char* Reservacion::getFechaPago() const {
+    return fechaPago;
+}
+
+float Reservacion::getMonto() const {
+    return monto;
+}
+
+const char* Reservacion::getAnotaciones() const {
+    return anotaciones;
+}
--- UdeAStay/reservacion.h
+++ UdeAStay/reservacion.h
@@ -1,15 +1,33 @@
 #ifndef RESERVACION_H
 #define RESERVACION_H
 
+#include "usuario.h"
 #include "alojamiento.h"
 
 class Reservacion
 {
 private:
     char* fechaEntrada;
-
+    int duracion;
+    char* codigoReservacaion;
+    Alojamiento* alojamiento;
+    Usuario* huesped;
+    char* metodoDePago;
+    char* fechadePago;
+    float monto;
+    char* anotaciones;
 public:
-    Reservacion();
+    Reservacion(const char* fechaEntrada, int duracion, const char* codigoReservacaion, Alojamiento* alojamiento, Usuario* huesped, const char* metodoDePago, const char* fechadePago, float monto, const char* anotaciones);
+    ~Reservacion();
+    const char* getFechaEntrada();
+    int getDuracion();
+    const char* getCodigoReservacion();
+    Alojamiento* getAlojamiento();
+    Usuario* getHuesped();
+    const char* getMetodoDePago();
+    const char* getFechaDePago();
+    float getMonto();
+    const char* getAnotaciones();
 };
 
 #endif // RESERVACION_H
